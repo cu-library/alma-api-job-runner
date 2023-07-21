894ca8e002d9cf05dc4b893ce5f6ba33cb9f37be
Kevin Bowrin <kevinbowrin@cunet.carleton.ca>
2023-07-21 14:28:08 -0400

    Update copyright date

--- almaerror.go
+++ almaerror.go
@@ -1,4 +1,4 @@
-// Copyright 2021 Carleton University Library All rights reserved.
+// Copyright 2023 Carleton University Library All rights reserved.
 // Use of this source code is governed by the MIT
 // license that can be found in the LICENSE file.
 
